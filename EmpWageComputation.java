76ce129d00a272b6647b31894b56e5ebb69bfd33
vamshi7589 <vamshi7589@gmail.com>
2020-11-15 15:48:04 +0530

    first commit

---
+++ EmpWageComputation.java
@@ -0,0 +1,16 @@
+public class EmployeeWageComputation
+{
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		int isFullTime=1;
+		int empCheck = (int)Math.floor(Math.random()*10)%2;
+		if(empCheck == isFullTime)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
+	}
+}
