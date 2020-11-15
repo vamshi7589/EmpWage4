2ba570776f7e3b54263cad47529d1c8bcf5f0eb6
vamshi7589 <vamshi7589@gmail.com>
2020-11-15 16:06:45 +0530

    added: added employee wage calculation program

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -8,17 +8,22 @@ public class EmployeeWageComputation
 		int dailyWage=0;
 		final int isFullTime=1;
 		final int isPartTime=2;
-		int i=0;
+		int days=0;
 		int monthlyWage=0;
+		int currentWorkingHours=0;
+		final int maxWorkingHours=100;
 		final int totMonthDays=20;
-		for(i=1;i<=totMonthDays;i++) {
+		while( currentWorkingHours<=maxWorkingHours || days<=totMonthDays) {
 			int empCheck = (int)Math.floor(Math.random()*10)%3;
+			days++;
 			switch (empCheck) {
 			case isFullTime:
 				dailyWage=(wagePerHour*fullDayHour);
+				currentWorkingHours+=fullDayHour;
 				break;
 			case isPartTime:
 				dailyWage=(wagePerHour*partTimeHour);
+				currentWorkingHours+=partTimeHour;
 				break;
 			default:
 				break;
