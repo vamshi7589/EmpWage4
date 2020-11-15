fa9f4a57cf7f304205baa42fce4d520430307f1f
vamshi7589 <vamshi7589@gmail.com>
2020-11-15 15:54:53 +0530

    added: added daily employee wage program

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,16 +1,20 @@
 public class EmployeeWageComputation
 {
 	public static void main(String[] args) {
-		System.out.println("**Welcome to Employee Wage Computation**");
+		System.out.println("Welcome to Employee Wage Computation");
+		final int wagePerHour=20;
+		final int fulldayHour=8;
+		int dailyWage=0;
 		int isFullTime=1;
 		int empCheck = (int)Math.floor(Math.random()*10)%2;
-		if(empCheck == isFullTime)
-		{
+		if (empCheck == isFullTime){
 			System.out.println("Employee is Present");
+			dailyWage=(wagePerHour*fulldayHour);
+			System.out.println("daily wage is "+dailyWage);
 		}
-		else
-		{
+		else{
 			System.out.println("Employee is absent");
 		}
 	}
 }
+
