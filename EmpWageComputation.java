5b6399124367c58fa240e893a82d7355529fe87d
vamshi7589 <vamshi7589@gmail.com>
2020-11-15 16:03:56 +0530

    added: added employee wage calculation for month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -8,20 +8,23 @@ public class EmployeeWageComputation
 		int dailyWage=0;
 		final int isFullTime=1;
 		final int isPartTime=2;
-		int empCheck = (int)Math.floor(Math.random()*10)%3;
-		switch (empCheck) {
-		case isFullTime:
-			System.out.println("Employee is full time");
-			dailyWage=(wagePerHour*fullDayHour);
-			System.out.println("daily wage is "+dailyWage);
-			break;
-		case isPartTime:
-			System.out.println("Employee is part time");
-			dailyWage=(wagePerHour*partTimeHour);
-			System.out.println("daily wage is "+dailyWage);
-			break;
-		default:
-			System.out.println("Employee is absent");
+		int i=0;
+		int monthlyWage=0;
+		final int totMonthDays=20;
+		for(i=1;i<=totMonthDays;i++) {
+			int empCheck = (int)Math.floor(Math.random()*10)%3;
+			switch (empCheck) {
+			case isFullTime:
+				dailyWage=(wagePerHour*fullDayHour);
+				break;
+			case isPartTime:
+				dailyWage=(wagePerHour*partTimeHour);
+				break;
+			default:
+				break;
+			}
+			monthlyWage+=dailyWage;
 		}
+		System.out.println("Total monthly salary is "+monthlyWage);
 	}
 }
