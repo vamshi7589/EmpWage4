52163ed7e6c3439546604fb4169803d695a67015
vamshi7589 <vamshi7589@gmail.com>
2020-11-15 15:51:04 +0530

    added: added employee attendance program

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,7 @@
 public class EmployeeWageComputation
 {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
+		System.out.println("**Welcome to Employee Wage Computation**");
 		int isFullTime=1;
 		int empCheck = (int)Math.floor(Math.random()*10)%2;
 		if(empCheck == isFullTime)
