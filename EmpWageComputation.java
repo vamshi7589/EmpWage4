b1b52dcad3bd7be5bfa0b09a68cc02ac3252df4c
vamshi7589 <vamshi7589@gmail.com>
2020-11-15 16:10:03 +0530

    added: added refactor program using methods

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,35 +1,71 @@
 public class EmployeeWageComputation
 {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
-		final int wagePerHour=20;
-		final int fullDayHour=8;
-		final int partTimeHour=4;
-		int dailyWage=0;
-		final int isFullTime=1;
-		final int isPartTime=2;
-		int days=0;
-		int monthlyWage=0;
-		int currentWorkingHours=0;
-		final int maxWorkingHours=100;
-		final int totMonthDays=20;
-		while( currentWorkingHours<=maxWorkingHours || days<=totMonthDays) {
-			int empCheck = (int)Math.floor(Math.random()*10)%3;
-			days++;
-			switch (empCheck) {
+	final int isFullTime=1;
+	final int isPartTime=2;
+	final int partTimeHours=4;
+	final int fullTimeHours=8;
+	final int empRatePerHour=20;
+	final int maxHours=100;
+        final int maxDays=20;
+
+	public int empAttendance() {
+		int empCheck=(int) Math.floor(Math.random()*10)%3;
+		return empCheck;
+	}
+	public void welcomeMessage() {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+	public void checkAttendence(){
+		switch (empAttendance()) {
+		case isFullTime:
+			System.out.println("Employee is full time");
+			break;
+		case isPartTime:
+			System.out.println("Employee is part time");
+			break;
+		default:
+			System.out.println("Employee is absent");
+		}
+	}
+	public int daliyWage(int n) {
+		int empWage=0;
+		switch (n) {
+		case isFullTime:
+			empWage = (empRatePerHour*fullTimeHours);
+			break;
+		case isPartTime:
+			empWage= (empRatePerHour*partTimeHours);
+			break;
+		default:
+			break;
+		}
+		return empWage;
+	}
+	public int monthlyWage() {
+		int days = 0;
+		int hours=0;
+		int monthlyWage = 0;
+		while (hours<=maxHours || days<=maxDays) {
+			int n=empAttendance();
+			switch (n) {
 			case isFullTime:
-				dailyWage=(wagePerHour*fullDayHour);
-				currentWorkingHours+=fullDayHour;
+				monthlyWage+=daliyWage(n);
+				hours+=fullTimeHours;
 				break;
 			case isPartTime:
-				dailyWage=(wagePerHour*partTimeHour);
-				currentWorkingHours+=partTimeHour;
+				monthlyWage+=daliyWage(n);
+				hours+=partTimeHours;
 				break;
 			default:
 				break;
 			}
-			monthlyWage+=dailyWage;
+			days++;
 		}
-		System.out.println("Total monthly salary is "+monthlyWage);
+		return monthlyWage;
+	}
+	public static void main(String[] args) {
+		EmployeeWageComputation e = new EmployeeWageComputation();
+		e.welcomeMessage();
+		System.out.println("Monthly Wage is "+e.monthlyWage()); 
 	}
 }
